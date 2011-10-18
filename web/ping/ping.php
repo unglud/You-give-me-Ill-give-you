5eee3c09a36eb13de0fb163c4a60dce8fb2cbc27
Alexander Matrosov <unglud@gmail.com>
2011-10-18 12:00:03 +0400

    ping 9

--- web/ping/ping.php
+++ web/ping/ping.php
@@ -2,5 +2,5 @@
 $sha = $_GET['s'];
 if($sha == '3b7372d20efe03c2816c9d1749bbb17374acc213' || (isset($_GET['t']) && $sha==$_GET['t'])){
 echo 'echo "test: '.$sha.'";';
-} else 'echo "Site is online.";';
+} else echo "Site is online.";
 ?>
